8d1e7a35d0cdaebc7c5a7347cad9eeef4dd97019
Artem Danich <gothmogx@gmail.com>
2025-03-21 21:06:47 +0100

    add ui blocker controller

---
+++ UIBlocker/Scripts/UIBlockerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GothmogToolkit.UIBlocker
+{
+	public class UIBlockerController : IUIBlockerController
+	{
+		public bool IsBlocked { private set; get; }
+		private bool CanBeBlocked => !IsBlocked;
+		private bool CanBeUnblocked => IsBlocked;
+
+		public event Action<bool> BlockedChanged;
+
+		public bool TryBlock()
+		{
+			if (!CanBeBlocked)
+				return false;
+			SetBlocked(true);
+			return true;
+		}
+
+		public bool TryUnblock()
+		{
+			if (!CanBeUnblocked)
+				return false;
+			SetBlocked(false);
+			return true;
+		}
+
+		private void SetBlocked(bool isBlocked)
+		{
+			if (isBlocked == IsBlocked)
+				return;
+			IsBlocked = isBlocked;
+			BlockedChanged?.Invoke(IsBlocked);
+		}
+	}
+}
