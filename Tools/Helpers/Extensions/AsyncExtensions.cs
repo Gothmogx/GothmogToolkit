b950a95a0e671446e373a21440272823fffd6e15
Artem Danich <gothmogx@gmail.com>
2025-03-23 11:04:25 +0100

    add async extensions

---
+++ Tools/Helpers/Extensions/AsyncExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace GothmogToolkit.Tools.Helpers.Extensions
+{
+	public static class AsyncExtensions
+	{
+		public static void CancelAndDispose(this CancellationTokenSource cts)
+		{
+			if (cts == null)
+				return;
+			
+			try
+			{
+				cts.Cancel();
+				cts.Dispose();
+			}
+			catch (ObjectDisposedException) { }
+		}
+	}
+}
