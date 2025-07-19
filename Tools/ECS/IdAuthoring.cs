adb5da9f56064dcc1f94aeb63c750f4dbfdad855
Artem Danich <gothmogx@gmail.com>
2025-07-19 13:40:31 +0200

    add id system

---
+++ Tools/ECS/IdAuthoring.cs
@@ -0,0 +1,37 @@
+﻿#if ECS
+using Unity.Entities;
+using UnityEngine;
+
+namespace GothmogToolkit.Tools.ECS
+{
+	public struct IdComponent : IComponentData
+	{
+		public uint Id;
+		public IdComponent(uint id)
+		{
+			Id = id;
+		}
+		public bool IsValid => Id != 0;
+	}
+
+	public struct NewIdTag : IComponentData
+	{
+	}
+	public class IdAuthoring : MonoBehaviour
+	{
+		[SerializeField] private uint _id;
+
+		private class Baker : Baker<IdAuthoring>
+		{
+			public override void Bake(IdAuthoring authoring)
+			{
+				var entity = GetEntity(TransformUsageFlags.None);
+				AddComponent(entity, new IdComponent(authoring._id));
+				AddComponent(entity, new NewIdTag());
+			}
+		}
+
+	}
+
+#endif
+}
---
+++ Tools/ECS/IdSystem.cs
@@ -0,0 +1,53 @@
+﻿#if ECS
+using GothmogToolkit.Tools.Helpers.ECS;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace GothmogToolkit.Tools.ECS
+{
+	public partial class IdSystem : SystemBase
+	{
+		private const int InitialCapacity = 1024;
+		private NativeHashMap<uint, Entity> _entities;
+		private uint _lastId;
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			_entities = new NativeHashMap<uint, Entity>(InitialCapacity, Allocator.Persistent);
+		}
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			_entities.Dispose();
+		}
+		protected override void OnUpdate()
+		{
+			var ecb = new EntityCommandBuffer(Allocator.Temp);
+			foreach (var (idComponent, newIdTag, entity) in SystemAPI.Query<RefRW<IdComponent>, RefRO<NewIdTag>>().WithEntityAccess())
+			{
+				if (TryAddId(idComponent.ValueRO, entity))
+				{
+					entity.RemoveComponent<NewIdTag>();
+					continue;
+				}
+
+				var newId = ++_lastId;
+
+				while (!_entities.TryAdd(newId, entity))
+				{
+					newId = ++_lastId;
+				}
+				_lastId = newId;
+
+				idComponent.ValueRW.Id = newId;
+				entity.RemoveComponent<NewIdTag>(ecb);
+			}
+			ecb.Playback(EntityManager);
+			ecb.Dispose();
+		}
+
+		private bool TryAddId(IdComponent idComponent, Entity entity)
+			=> idComponent.IsValid && _entities.TryAdd(idComponent.Id, entity);
+	}
+}
+#endif
