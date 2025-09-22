68a0a774c757cc84d801f9bdf16fc41afa06203e
Artem Danich <gothmogx@gmail.com>
2025-09-22 17:23:26 +0200

    add generic staes handler machine

--- Tools/Core/StatesHandler/Handler/StatesHandlerMachine.cs
+++ Tools/Core/StatesHandler/Handler/StatesHandlerMachine.cs
@@ -5,7 +5,8 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 
 namespace GothmogToolkit.Tools.Core.StatesHandler
-{
+{ 
+	[Obsolete("Use generic version")]
 	public class StatesHandlerMachine
 	{
 		private readonly Dictionary<Type, IState> _states = new(8);
@@ -35,7 +36,7 @@ namespace GothmogToolkit.Tools.Core.StatesHandler
 		}
 
 		public async UniTask Run<TFirstState>(CancellationToken cancellationToken, bool shouldYieldBetweenStates = false)
-			where TFirstState : State
+			where TFirstState : IState
 		{
 			var nextState = typeof(TFirstState);
 			StateTransition lastTransition = null;
@@ -69,5 +70,70 @@ namespace GothmogToolkit.Tools.Core.StatesHandler
 
 		public bool TryGetState(Type type, out IState state) => _states.TryGetValue(type, out state);
 	}
+	
+	public class StatesHandlerMachine<TState> where TState : IState
+	{
+		private readonly Dictionary<Type, TState> _states = new(8);
+
+		public event Action<TState> EnteringState;
+		public event Action<TState> ExitedState;
+
+		public void Initialize(IEnumerable<TState> states)
+		{
+			foreach (var state in states)
+			{
+				RegisterState(state);
+			}
+		}
+
+		private void RegisterState(TState state)
+		{
+			if (state == null)
+				throw new ArgumentNullException($"Failed to register state. The state is null.");
+			
+			if (state.Type == null)
+				throw new ArgumentNullException($"Failed to register state. Type of the state {state} is null.");
+			
+			if (!_states.TryAdd(state.Type, state))
+				throw new ArgumentException(
+					$"Failed to register state. State of type {state.Type} is already registered");
+		}
+
+		public async UniTask Run<TFirstState>(CancellationToken cancellationToken, bool shouldYieldBetweenStates = false)
+			where TFirstState : TState
+		{
+			var nextState = typeof(TFirstState);
+			StateTransition lastTransition = null;
+
+			while (nextState != null)
+			{
+				if (!TryGetState(nextState, out var state))
+					throw new ArgumentException($"No state of type {nextState} found");
+
+				try
+				{
+					cancellationToken.ThrowIfCancellationRequested();
+					
+					EnteringState?.Invoke(state);
+					lastTransition = await state.Execute(cancellationToken, lastTransition?.TransitionArgs);
+					ExitedState?.Invoke(state);
+				}
+				catch (OperationCanceledException)
+				{
+					return;
+				}
+
+				nextState = lastTransition.NextStateType;
+				if (lastTransition?.NextStateType == null)
+					return;
+				
+				if(shouldYieldBetweenStates)
+					await UniTask.Yield();
+			}
+		}
+
+		public bool TryGetState(Type type, out TState state) => _states.TryGetValue(type, out state);
+	}
+	
 }
 #endif
