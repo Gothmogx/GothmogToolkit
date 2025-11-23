4fc3d2b6efa9bc346b0b4c731f711c965357ad3c
Artem Danich <gothmogx@gmail.com>
2025-11-23 20:37:36 +0100

    add prefab utils

---
+++ Tools/EditorTools/Other/PrefabUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GothmogToolkit.Tools.EditorTools.Other
+{
+	public static class PrefabUtils
+	{
+		#if UNITY_EDITOR
+		public static T InstantiatePrefab<T>(T prefab, Transform transform) where T : Component
+			=> UnityEditor.PrefabUtility.InstantiatePrefab(prefab, transform) as T;
+		#else 
+		public static T InstantiatePrefab<T>(T prefab, Transform transform) where T : Component  => null;
+		#endif
+	}
+}
